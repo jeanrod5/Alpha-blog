6f414abe40181b3ec570075b9561dd9f647dab61
James <jeancarlos19gc@gmail.com>
2018-09-18 15:04:32 +0000

    Add users listing and update show article view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  def non_footer_action
+  do_stuff
+  @skip_footer = true
+  end
 end
