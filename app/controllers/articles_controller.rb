429beca8427283cb2b97a279354d1fe885045d28
James <jeancarlos19gc@gmail.com>
2018-09-19 16:06:01 +0000

    added authentication methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,7 +4,7 @@ class ArticlesController < ApplicationController
     
     
     def index
-       @articles = Article.all  
+       @articles = Article.paginate(page: params[:page], per_page: 2)
     end
     
     def new
